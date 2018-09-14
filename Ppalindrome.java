ea687af40d7f820f50ebae86a69fea1961864d15
chokkuu1998 <chokkuu1998@gmail.com>
2018-09-14 14:15:38 +0530

    Rename Palindrome.java to Ppalindrome.java

---
+++ Ppalindrome.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Palindrome
+{
+ public static void main(String args[])
+ {
+   int n, m, a = 0,x;
+    Scanner s = new Scanner(System.in);
+    System.out.print("Enter any number:");
+   n = s.nextInt();
+   m = n;
+   while(n > 0)
+    {
+     x = n % 10;
+       a = a * 10 + x;
+       n = n / 10;
+     }
+    if(a == m)
+    {
+     System.out.println("Given number "+m+" is Palindrome");
+     }
+    else
+    {
+      System.out.println("Given number "+m+" is Not Palindrome");
+     }
+    }
+}
