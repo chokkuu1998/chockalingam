3ef9f12353b9c5338f4c741d8fdc33bde51456f3
chokkuu1998 <chokkuu1998@gmail.com>
2018-09-14 14:00:13 +0530

    Create power.java

---
+++ power.java
@@ -0,0 +1,13 @@
+public class Power
+{
+public static void main(String[] args)
+{
+int base = 3, exponent = 4;
+long result = 1
+for (;exponent != 0; --exponent)
+{
+ result *= base;
+ }
+ System.out.println("Answer = " + result);
+}
+}
