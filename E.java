67e165d4a8a7fcb7691afd61bfe7d0d711e5cd20
chokkuu1998 <chokkuu1998@gmail.com>
2018-09-14 13:50:50 +0530

    Create E.java

---
+++ E.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class E {
+ public static void main(String[] args)
+{
+ Scanner in = new Scanner(System.in);
+   System.out.print("Input a number: ");
+  int n = in.nextInt();
+ if (n > 0)
+   {
+    System.out.println("Number is positive");
+   }
+   else if (n < 0)
+  {
+  System.out.println("Number is negative");
+  }
+    else
+ {
+System.out.println("Number is zero");
+        }
+    }
+}
