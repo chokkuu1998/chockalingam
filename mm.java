2994aa1858f8885f1ef26e31644c75da2349660c
chokkuu1998 <chokkuu1998@gmail.com>
2018-12-28 14:44:14 +0530

    Create mm.java

---
+++ mm.java
@@ -0,0 +1,11 @@
+public class Clock
+{
+public int seconds;
+public int minutes;
+ public int hours;
+ public Clock ( int InsertSeconds, int InsertMinutes, int InsertHours){
+ seconds = InsertSeconds;
+  minutes = InsertMinutes;
+ hours   = InsertHours;
+ }
+ }
