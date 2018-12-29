f93b9618ea00b83b2c3f3988e2fc8ad2c98ef3f6
chokkuu1998 <chokkuu1998@gmail.com>
2018-12-29 20:37:22 +0530

    Create pill.java

---
+++ pill.java
@@ -0,0 +1,18 @@
+public class ListEvenNumbers {
+ 
+	public static void main(String[] args) {
+		
+	
+		int limit = 50;
+		
+		System.out.println("Printing Even numbers between 1 and " + limit);
+		
+		for(int i=1; i <= limit; i++){
+			
+			
+			if( i % 2 == 0){
+				System.out.print(i + " ");
+			}
+		}
+	}
+}
