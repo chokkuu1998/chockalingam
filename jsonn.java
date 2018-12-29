df7d3dee254a3a56bb6e5227e2dff92b41cf555d
chokkuu1998 <chokkuu1998@gmail.com>
2018-12-29 20:16:41 +0530

    Create jsonn.java

---
+++ jsonn.java
@@ -0,0 +1,16 @@
+public class NumberDigits {
+
+    public static void main(String[] args) {
+
+        int count = 0, num = 3452;
+
+        while(num != 0)
+        {
+            // num = num/10
+            num /= 10;
+            ++count;
+        }
+
+        System.out.println("Number of digits: " + count);
+    }
+}
