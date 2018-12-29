f09dd77d47462cf3d12707fcda0046ab1decd8a7
chokkuu1998 <chokkuu1998@gmail.com>
2018-12-29 20:44:35 +0530

    Create ni.java

---
+++ ni.java
@@ -0,0 +1,18 @@
+public class FindEvenOrOddNumber {
+ 
+	public static void main(String[] args) {
+		
+		//create an array of 10 numbers
+		int[] numbers = new int[]{1,2,3,4,5,6,7,8,9,10};
+		
+		for(int i=0; i < numbers.length; i++)
+    {
+			 	 if(numbers[i]%2 == 0)
+			 	System.out.println(numbers[i] + " is even number.");
+			 else
+			 	System.out.println(numbers[i] + " is odd number.");
+			 	
+		}
+		
+	}
+}
