4ca1b89095b91376dc26ce3ddf686cc36a440582
chokkuu1998 <chokkuu1998@gmail.com>
2018-12-29 19:48:32 +0530

    Create aa.java

---
+++ aa.java
@@ -0,0 +1,18 @@
+public class JavaExample {
+
+    public static void main(String[] args) {
+        double[] arr = {19, 12.89, 16.5, 200, 13.7};
+        double total = 0;
+
+        for(int i=0; i<arr.length; i++){
+        	total = total + arr[i];
+        }
+        double average = total / arr.length;
+        
+        /* This is used for displaying the formatted output
+         * if you give %.4f then the output would have 4 digits
+         * after decimal point.
+         */
+        System.out.format("The average is: %.3f", average);
+    }
+}
