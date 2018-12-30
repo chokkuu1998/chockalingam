28850884e5619bb282e54f3e5bb2f085ced8eee2
chokkuu1998 <chokkuu1998@gmail.com>
2018-12-30 10:18:52 +0530

    Create www.java

---
+++ www.java
@@ -0,0 +1,9 @@
+ int l = 0;
+      int m = 5;
+      for(int i = 0; i < (string.length()/5) ; i++)
+      {
+            System.out.println(string.substring(j,k));
+            l = m;
+            m = m + 5;
+
+      }
