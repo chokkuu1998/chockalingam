43edc1f49a55bd8431fb64e1d607152c7d042ce6
chokkuu1998 <chokkuu1998@gmail.com>
2018-12-30 10:14:20 +0530

    Create cv.java

---
+++ cv.java
@@ -0,0 +1,17 @@
+public class Natural 
+{
+public static void main(String args[])
+{
+int x, i = 1 ;
+int sum = 0;
+System.out.println("Enter Number of items :");
+Scanner s = new Scanner(System.in);
+x = s.nextInt();
+while(i <= x)
+{
+sum = sum +i;
+i++;
+}
+System.out.println("Sum of "+x+" numbers is :"+sum);
+} 
+}
