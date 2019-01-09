92777e9f130a06b40280d9ee83de39ada8611774
chokkuu1998 <chokkuu1998@gmail.com>
2019-01-09 14:28:33 +0530

    Create gfg.java

---
+++ gfg.java
@@ -0,0 +1,28 @@
+import java.lang.Math; 
+  
+class Gfg
+{ 
+  
+  
+    public static void main(String args[]) 
+    { 
+        double a = 4.3; 
+        double b = 1.0 / 0; 
+        double c = 0.0; 
+        double d = -0.0; 
+        double e = -2.3; 
+  
+        System.out.println(Math.floor(a)); 
+  
+   
+        System.out.println(Math.floor(b)); 
+   
+        System.out.println(Math.floor(c)); 
+  
+
+        System.out.println(Math.floor(d)); 
+  
+       
+        System.out.println(Math.floor(e)); 
+    } 
+} 
