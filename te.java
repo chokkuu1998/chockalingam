793dd573a00262ccb5c4e4bb8d6f1380e3e7dcaa
chokkuu1998 <chokkuu1998@gmail.com>
2019-01-11 11:44:22 +0530

    Create te.java

---
+++ te.java
@@ -0,0 +1,10 @@
+public class Test
+{ 
+    public static void main(String[] args) 
+    { 
+        String s1 = new String("HELLO"); 
+        String s2 = new String("HELLO"); 
+        System.out.println(s1 == s2); 
+        System.out.println(s1.equals(s2)); 
+    } 
+} 
