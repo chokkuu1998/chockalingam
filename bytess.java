abf8b41d10fd534d0fd8e37fb7906853b7838306
chokkuu1998 <chokkuu1998@gmail.com>
2019-01-14 08:24:11 +0530

    Create bytess.java

---
+++ bytess.java
@@ -0,0 +1,22 @@
+{
+public static void main(String args{})
+{
+largerst value n= 2;
+
+smallest value m= 100
+for (int n= 2; n < m; n++)
+
+System.out.println(n);
+
+{
+
+if (smallest value n= 2)
+
+System.out.println(m);
+
+if (largest value m= 100)
+
+System.out.println (m= 100)
+
+}
+}
