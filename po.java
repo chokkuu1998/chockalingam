0e079ee3b822f9ab868ecd7d459f329c68a5dfde
chokkuu1998 <chokkuu1998@gmail.com>
2019-01-20 10:03:46 +0530

    Create po.java

---
+++ po.java
@@ -0,0 +1,15 @@
+public class test 
+{
+    public static void main(String[] args) 
+    {
+        double x;`enter code here`
+        x = 3411.999999999999;
+        System.out.println("16: "+x);   
+        x = 3411.9999999999999;
+        System.out.println("17: "+x);  
+        x = 0.9999999999999999;
+        System.out.println("16: "+x):
+        x = 0.99999999999999999;
+        System.out.println("17: "+x);  
+    }  
+}
