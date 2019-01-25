5634506a29e9e05da8e139261038eb9b0a641851
chokkuu1998 <chokkuu1998@gmail.com>
2019-01-25 11:39:46 +0530

    Create j4.java

---
+++ j4.java
@@ -0,0 +1,25 @@
+import java.util.*; 
+ 
+class j4
+{ 
+	public static void main(String args[]) 
+	{ 
+		int a,b,sum; 
+		float avg; 
+
+
+		Scanner buf=new Scanner(System.in); 
+
+		System.out.print("Enter first number : "); 
+		a=buf.nextInt(); 
+
+		System.out.print("Enter second number : "); 
+		b=buf.nextInt(); 
+
+		/*Calculate sum and average*/
+		sum = a+b; 
+		avg = (float)((a+b)/2); 
+
+		System.out.print("Sum : " +sum +"\nAverage : " +avg); 
+	} 
+}
