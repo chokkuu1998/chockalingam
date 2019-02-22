990c8ae6e137cd5eaff8760db834f41bc7bb48f3
chokkuu1998 <chokkuu1998@gmail.com>
2019-02-22 14:30:23 +0530

    Create 64.py

---
+++ 64.py
@@ -0,0 +1,21 @@
+def card():
+    o = int(input())
+    z = int(input())
+    n = o - z
+    if n >= 3 or (-n) >= 3:
+        print(-1)
+        return
+    l = ['0', '1']
+    s = ''
+    for i in range(0, o + z, 2):
+        s += l[1]
+        if i == (o + z) - 1 and i % 2 != 1:
+            break
+        s += l[0]
+    print(s)
+
+
+try:
+    card()
+except:
+    print('invalid')
