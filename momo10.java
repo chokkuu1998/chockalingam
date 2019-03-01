8c488e2315ab2a311f62b4e701c031315c83c095
chokkuu1998 <chokkuu1998@gmail.com>
2019-03-01 11:45:25 +0530

    Create momo10.java

---
+++ momo10.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class testing 
+{
+    public static void main(String [] arg) 
+    {   
+        int[] src = new int[] {1, 2, 3, 4, 5}; 
+        int b1[] = Arrays.copyOfRange(src, 0, 2);
+    }
+}
