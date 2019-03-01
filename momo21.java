2763e5e51ba673e71ffa21f004e64951d75e428c
chokkuu1998 <chokkuu1998@gmail.com>
2019-03-01 12:18:54 +0530

    Create momo21.java

---
+++ momo21.java
@@ -0,0 +1,8 @@
+public static void main(String[] args) 
+{
+		String str = "PANKAJ";
+		byte[] byteArr = str.getBytes();
+	
+		System.out.println("String to byte array: " + Arrays.toString(byteArr));
+	}
+}
