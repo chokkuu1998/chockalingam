3708897bd0c0562fe887e99681145ab97b9b38cf
chokkuu1998 <chokkuu1998@gmail.com>
2019-03-01 11:35:58 +0530

    Create momo6.java

---
+++ momo6.java
@@ -0,0 +1,12 @@
+public class Gfg 
+{ 
+  
+    public static void main(String args[]) 
+    { 
+        int a = 23; 
+        int b = -23; 
+  
+     
+        System.out.println(Math.max(a, b)); 
+    } 
+} 
