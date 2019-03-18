9a0a5ae7c191c5bc87da99cfa4e1c51872555aba
chokkuu1998 <chokkuu1998@gmail.com>
2019-03-18 10:41:05 +0530

    Create 101.java

---
+++ 101.java
@@ -0,0 +1,9 @@
+public class Test
+{ 
+public static void main(String[] args) 
+{ int[] array = new int[4]; 
+System.out.println("The size of the array is " + array.length); 
+String str = "GeeksforGeeks"; 
+System.out.println("The size of the String is " + str.length()); 
+} 
+} 
