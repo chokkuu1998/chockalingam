f203e5d2340f226c1fe3607c202de379469d57b4
chokkuu1998 <chokkuu1998@gmail.com>
2019-03-18 10:52:41 +0530

    Create 107.java

---
+++ 107.java
@@ -0,0 +1,19 @@
+ public static void main(String[] args)
+    {
+        int smallest=0;
+        int large=0; 
+        int num;
+
+        System.out.println("enter the number");
+
+        Scanner input=new Scanner(System.in);
+
+        int n=input.nextInt();
+
+        for(int i=0;i<n;i++)
+        {
+          num=input.nextInt();
+          if(num>large)
+          {
+           large=num;
+          }
