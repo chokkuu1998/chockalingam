84c8b1b6735001df64c3dfafc43fe11677637be1
chokkuu1998 <chokkuu1998@gmail.com>
2019-03-18 10:57:32 +0530

    Create 109.java

---
+++ 109.java
@@ -0,0 +1,13 @@
+public class CompareToExample
+{ 
+public static void main(String args[])
+{ 
+String s1="hello";
+String s2="hello"; 
+String s3="hemlo"; 
+String s4="flag";
+System.out.println(s1.compareTo(s2)); 
+System.out.println(s1.compareTo(s3)); 
+System.out.println(s1.compareTo(s4));
+}
+} 
