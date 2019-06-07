b1ca2fcfa9f30271cdf44484c09380aaafdf6245
chokkuu1998 <chokkuu1998@gmail.com>
2019-06-07 14:48:37 +0530

    Rename m14.java to m15.java

---
+++ m15.java
@@ -0,0 +1,12 @@
+public class Gfg 
+{ 
+  
+    public static void main(String args[]) 
+    { 
+        double a = 12.123; 
+        double b = 12.456; 
+  
+        
+        System.out.println(Math.max(a, b)); 
+    } 
+} 
