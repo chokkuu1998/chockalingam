9c67ab007f78341b11ab3abdc2147d648a1cc3a9
chokkuu1998 <chokkuu1998@gmail.com>
2019-06-07 11:25:27 +0530

    Create m11.java

---
+++ m11.java
@@ -0,0 +1,15 @@
+class Gfg1
+{ 
+  
+  
+    public static void main(String args[]) 
+    { 
+  
+        int l = 10; 
+        
+        System.out.println("Binary is " + Integer.toBinaryString(l)); 
+  
+        l = 9; 
+        System.out.println("Binary is " + Integer.toBinaryString(l)); 
+    } 
+} 
