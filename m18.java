a9bcb644f01eca016540ca5f5d4acb2929aa53b1
chokkuu1998 <chokkuu1998@gmail.com>
2019-06-07 14:54:47 +0530

    Create m18.java

---
+++ m18.java
@@ -0,0 +1,19 @@
+public class Average {  
+   public static void main(String[] args)  
+    {  
+      int n, count = 1;   
+      float  xF, averageF, sumF = 0;   
+      Scanner sc = new Scanner(System.in);     
+      System.out.println("Enter the value of n");  
+      n = sc.nextInt();  
+      while (count <= n)   
+             {   
+                  System.out.println("Enter the "+count+" number?");  
+                  xF = sc.nextInt();  
+                  sumF += xF;   
+                  ++count;   
+             }   
+                  averageF = sumF/n;   
+        System.out.println("The Average is"+averageF);  
+    }    
+}  
