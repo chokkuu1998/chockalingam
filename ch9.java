e5d3bb650ee1bc9a5dd22f667055ad0ccdba0fd9
chokkuu1998 <chokkuu1998@gmail.com>
2019-06-12 09:23:24 +0530

    Create ch9.java

---
+++ ch9.java
@@ -0,0 +1,16 @@
+public class WriteStringToFile1 {
+	
+	public static void main(String[] args) {	
+		try {
+			File file = new File("test1.txt");
+			FileWriter fileWriter = new FileWriter(file);
+			fileWriter.write("This is ");
+			fileWriter.write("a test");
+			fileWriter.flush();
+			fileWriter.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
