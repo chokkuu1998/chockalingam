c96115a26455d80b2ecb04e572c1f13c0f13d49d
chokkuu1998 <chokkuu1998@gmail.com>
2019-06-12 10:13:51 +0530

    Create ch19.java

---
+++ ch19.java
@@ -0,0 +1,20 @@
+class MultiplyDivide
+{
+
+  public static void main (String args[])
+  {
+
+    int i = 10;
+    int j = 2;
+
+    System.out.println("i is " + i);
+    System.out.println("j is " + j);
+  
+    int k = i/j;
+    System.out.println("i/j is " + k);
+    k = i * j;
+    System.out.println("i * j is " + k);
+
+  }
+
+}
